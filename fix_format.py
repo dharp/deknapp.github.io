87896c3b4d165d7c261eb52b524e2f46cddd6deb
Nathaniel Knapp <nknapp@lanl.gov>
2017-08-31 13:54:06 -0600

    changed html links to markdown links

--- fix_format.py
+++ fix_format.py
@@ -4,8 +4,9 @@ def filter_file(infile_name, character):
     outfile_name = infile_name[:-3] + '_temp.md'
     with open(infile_name, 'r') as infile, open(outfile_name, 'w') as outfile:
 	data = infile.read()
-	data = data.replace(character, "")
-	outfile.write(textwrap.dedent(data))
+	data = data.replace("html", "md")
+        data = data.replace("smd", "md")
+	outfile.write((data)
 
 dr = '/home/nknapp/deknapp.github.io/pages/'
 
@@ -16,6 +17,6 @@ dr = '/home/nknapp/deknapp.github.io/pages/'
 
 for root, drs, fles in os.walk(dr):
     for fle in fles:
-        if ".md" in fle and "temp" in fle:
+        if ".md" in fle:
             name = os.path.join(root, fle)
-            os.remove(name)
+            filter_file(name)
